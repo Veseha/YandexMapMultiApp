b5421800c82afaca1a238a69b771534ad5cc5ccc
lunar <someth183@gmail.com>
2020-04-14 18:21:20 +0600

    upd

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from PyQt5.QtWidgets import QApplication, QWidget
-from PyQt5.QtWidgets import QPushButton, QLineEdit,QLabel
+from PyQt5.QtWidgets import QPushButton, QLineEdit, QLabel
 from PyQt5.QtGui import QPixmap, QImage
 import sys
 from geocoderAPI import get_cords
@@ -14,7 +14,6 @@ def get_image_from_cords(req):
 
 
 get_image_from_cords(input('Input your adress'))
-
 
 SCREEN_SIZE = [400, 500]
 
@@ -34,7 +33,6 @@ class Example(QWidget):
         self.image1.resize(350, 350)
         self.image1.setPixmap(self.draw)
 
-
         self.rotateplus = QPushButton('Повернуть направо', self)
         self.rotateplus.setGeometry(10, 10, 160, 40)
         self.rotateplus.clicked.connect(self.rotate)
@@ -47,48 +45,52 @@ class Example(QWidget):
         self.r.setGeometry(10, 60, 50, 50)
         self.g.setGeometry(70, 60, 50, 50)
         self.b.setGeometry(130, 60, 50, 50)
-        self.r.clicked.connect(self.run)
-        self.g.clicked.connect(self.run)
-        self.b.clicked.connect(self.run)
+        self.r.clicked.connect(self.onClick)
+        self.g.clicked.connect(self.onClick)
+        self.b.clicked.connect(self.onClick)
 
         self.noup = QPushButton('ГДЕ МОИ ЦВЕТА?', self)
         self.noup.setGeometry(190, 60, 200, 50)
-        self.noup.clicked.connect(self.run)
+        self.noup.clicked.connect(self.onClick)
 
     def rotate(self):
-        im = Image.open(self.image)
-        if self.sender().text() == 'Повернуть направо':
-            rot = -90
-        else:
-            rot = 90
-        im1 = im.rotate(rot, expand=True)
-        im1.save('imagene.jpg')
-        self.draw.load('imagene.jpg')
-        self.image = 'imagene.jpg'
-        self.image1.setPixmap(self.draw)
+        try:
+            im = Image.open(self.image)
+            if self.sender().text() == 'Повернуть направо':
+                rot = -90
+            else:
+                rot = 90
+            im1 = im.rotate(rot, expand=True)
+            im1.save('imagene.jpg')
+            self.draw.load('imagene.jpg')
+            self.image = 'imagene.jpg'
+            self.image1.setPixmap(self.draw)
+        except Exception as e:
+            print('error in rotate(), stack:', e)
 
-    def run(self):
-        im = Image.open('main_image.png')
-        x, y = im.size
-        pix = im.load()
-        for i in range(x):
-            for j in range(y):
-                r, g, b = pix[i, j]
-                if self.sender().text() == 'R':
-                    pix[i, j] = r, 0, 0
-                elif self.sender().text() == 'G':
-                    pix[i, j] = 0, g, 0
-                elif self.sender().text() == 'B':
-                    pix[i, j] = 0, 0, b
-                else:
-                    pass
+    def onClick(self):
+        try:
+            im = Image.open('main_image.png')
+            x, y = im.size
+            pix = im.load()
+            for i in range(x):
+                for j in range(y):
+                    r, g, b = pix[i, j]
+                    if self.sender().text() == 'R':
+                        pix[i, j] = r, 0, 0
+                    elif self.sender().text() == 'G':
+                        pix[i, j] = 0, g, 0
+                    elif self.sender().text() == 'B':
+                        pix[i, j] = 0, 0, b
+                    else:
+                        pass
 
-        im.save('imagene.jpg')
-        self.draw.load('imagene.jpg')
-        self.image = 'imagene.jpg'
-        self.image1.setPixmap(self.draw)
-
-
+            im.save('imagene.jpg')
+            self.draw.load('imagene.jpg')
+            self.image = 'imagene.jpg'
+            self.image1.setPixmap(self.draw)
+        except Exception as e:
+            print('error in onClick(), stack:', e)
 
 
 if __name__ == '__main__':
